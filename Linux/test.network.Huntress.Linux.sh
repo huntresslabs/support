1fbf23351b711ab4a25425b3451d10217d77c18f
Alan Bishop [Huntress] <109673875+Alan-Huntress@users.noreply.github.com>
2025-11-17 12:49:01 -0500

    test for certificate issuer, updated URL's, added logging function, log script version

--- Linux/test.network.Huntress.Linux.sh
+++ Linux/test.network.Huntress.Linux.sh
@@ -1,48 +1,60 @@
 #!/bin/bash
 
 # Tests a number of ways Huntress agents communicate with the Huntress portal
-# Output is to standard out as well as the file huntress_network_test.log
+# Output is to standard out as well as the file represented by $DebugLog
 #
 # <<< Linux version >>>
 
+DebugLog="huntress_network_test.log"
+
+dd=$(date "+%Y-%m-%d  %H:%M:%S")
+logger() {
+    echo "$*";
+    echo "$dd -- $*" >> $DebugLog;
+}
+
+logger "Huntress Network Tester, Linux Bash, last updated Nov 17, 2025"
+logger ""
+
 #test connectivity to Huntress.io on port 80 - output should be <title>Huntress Management Console</title>
-echo "-- Testing DNS resolution and port 80 connectivity" | tee -a huntress_network_test.log 
+logger "-- Testing DNS resolution and port 80 connectivity"
 curlOutput=$(sudo curl https://huntress.io -s)
 if [[ "$curlOutput" == *"<title>Huntress Management Console</title>"* ]]; then
-     echo "[DNS Resolution / port 80 connection successful]" | tee -a huntress_network_test.log 
+     logger "[DNS Resolution / port 80 connection successful]"
 else
-     echo "[FAILED: DNS and port 80 checks]" | tee -a huntress_network_test.log 
+     logger "[FAILED: DNS and port 80 checks]"
 fi
-echo "" | tee -a huntress_network_test.log 
+logger ""
 
 #tests that the Huntress certificate is not intercepted. If the Huntress cert is not returned the agent will not function.
 #output should indicate the cert is for Huntress (should be the first entry, begins with 0)
-echo "-- Testing Certificate Validation --" | tee -a huntress_network_test.log 
-cert=$( openssl s_client -connect huntress.io:443 -servername huntress.io 2> /dev/null < /dev/null | head | grep "Huntress" ) 
-if [[ "$cert" == *"huntress.io"* ]]; then
-     echo "[Certificate validation successful]" | tee -a huntress_network_test.log 
+logger "-- Testing Certificate Validation --"
+cert=$( openssl s_client -connect huntress.io:443 -servername huntress.io 2> /dev/null < /dev/null | head | grep "Huntress" )
+issuer=$( openssl s_client -connect huntress.io:443 -servername huntress.io 2> /dev/null < /dev/null | head | grep "1 s:/C=US" )
+if [[ "$cert" == " 0 s:/C=US/ST=Maryland/L=Ellicott City/O=Huntress Labs Inc./CN=*.huntress.io" && "$issuer" == " 1 s:/C=US/O=DigiCert Inc/CN=DigiCert Global G2 TLS RSA SHA256 2020 CA1" ]]; then
+     logger "[Certificate validation successful]"
 else
-     echo "[FAILED: Certificate validation]" | tee -a huntress_network_test.log 
-     echo "$cert"
+     logger "[FAILED: Certificate validation]"
+     logger "Certificate that was returned: $cert $issuer"
 
 fi
-echo ""  | tee -a huntress_network_test.log 
+logger ""
 
 #test outgoing port 443 connectivity to Huntress
 #output should indicate every URL connection succeeded
 countFails=0
-echo "-- Verifying Huntress services can be reached --" | tee -a huntress_network_test.log 
-for hostn in "update.huntress.io" "huntress.io" "eetee.huntress.io" "huntress-installers.s3.amazonaws.com" "huntress-updates.s3.amazonaws.com" "huntress-uploads.s3.us-west-2.amazonaws.com" "huntress-user-uploads.s3.amazonaws.com" "huntress-rio.s3.amazonaws.com" "huntress-survey-results.s3.amazonaws.com"; do
+logger "-- Verifying Huntress services can be reached --"
+for hostn in "update.huntress.io" "huntress.io" "eetee.huntress.io" "eetee.huntresscdn.com" "huntresscdn.com" "huntress-installers.s3.amazonaws.com" "huntress-updates.s3.amazonaws.com" "huntress-uploads.s3.us-west-2.amazonaws.com" "huntress-user-uploads.s3.amazonaws.com" "huntress-rio.s3.amazonaws.com" "huntress-survey-results.s3.amazonaws.com" "huntress-log-uploads.s3.amazonaws.com"; do
      nc=$(nc -z -v $hostn 443 2>&1 | grep "succeeded")
      if [ -n "$nc" ]; then
-          echo "[Connection to $hostn successful]" | tee -a huntress_network_test.log 
+          logger "[Connection to $hostn successful]"
      else
-          echo "[FAILED: Connection to $hostn]" | tee -a huntress_network_test.log 
+          logger "[FAILED: Connection to $hostn]"
           countFails++
-     fi 
+     fi
 done
 if [ "$countFails" -gt 0 ]; then
-     echo "[FAILED to connect to all Huntress services]" | tee -a huntress_network_test.log 
+     logger "[FAILED to connect to all Huntress services]"
 else
-     echo "[Successfully connected to Huntress services]" | tee -a huntress_network_test.log 
+     logger "[Successfully connected to Huntress services]"
 fi
