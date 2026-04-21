44acdf4b686349996457cac9860c12c98126e769
Alan Bishop [Huntress] <109673875+Alan-Huntress@users.noreply.github.com>
2026-04-21 12:18:31 -0400

    Update Test.network.Huntress.sh
    Added new Huntress URLs for testing. 
    Pivoted to hashing the entire cert instead of just reading subject lines.

--- macOS/Test.network.Huntress.sh
+++ macOS/Test.network.Huntress.sh
@@ -13,43 +13,50 @@ logger() {
     echo "$dd -- $*" >> $DebugLog;
 }
 
-logger "Huntress Network Tester, macOS Bash, last updated Nov 17, 2025"
+logger "Huntress Network Tester, macOS Bash, last updated April 21, 2026"
 logger ""
+countFails=0
 
-#test connectivity to Huntress.io on port 80 - output should be <title>Huntress Management Console</title>
+# #test connectivity to Huntress.io on port 80 - output should be <title>Huntress Management Console</title>
 logger "-- Testing DNS resolution and port 80 connectivity"
 curlOutput=$(sudo curl https://huntress.io -s | head -n 14 | tail -n 1)
 if [ "$curlOutput" == "<title>Huntress Management Console</title>" ]; then
      logger "[DNS Resolution / port 80 connection successful]"
 else
      logger "[FAILED: DNS and port 80 checks]"
+     countFails++
 fi
 logger ""
 
-#tests that the Huntress certificate is not intercepted. If the Huntress cert is not returned the agent will not function.
-#output should indicate the cert is for Huntress (should be the first entry, begins with 0)
+
+#tests that the expected certificates are not intercepted. If the expected cert is not returned the agent will not function.
+#output should indicate the cert is for Huntress or Microsoft 
 logger "-- Testing Certificate Validation --"
-cert=" 0 s:/C=US/ST=Maryland/L=Ellicott City/O=Huntress Labs Inc./CN=*.huntress.io"
-certResponse=$( openssl s_client -connect huntress.io:443 -servername huntress.io 2> /dev/null < /dev/null | head | grep "Huntress" )
-issuer=" 1 s:/C=US/O=DigiCert Inc/CN=DigiCert Global G2 TLS RSA SHA256 2020 CA1"
-issuerResponse=$( openssl s_client -connect huntress.io:443 -servername huntress.io 2> /dev/null < /dev/null | head | grep "1 s:/C=US" )
-if [[ "$cert" == "$certResponse" && "$issuer" == "$issuerResponse" ]]; then
-     logger "[Certificate validation successful]"
-else
-     logger "[FAILED: Certificate validation]"
-     logger "Certificate that was returned: $certResponse"
-     logger "Certificate that was expected: $cert"
-     logger "Issuer that was returned: $issuerResponse"
-     logger "Issuer that was expected: $issuer"
-fi
+hash=("87:5B:E4:BB:1B:08:7F:57:3E:EB:4D:F0:C6:CB:81:1F:07:78:B0:E0:EF:9E:2F:31:84:EA:9F:2A:25:88:47:5C" "75:A9:87:63:3E:2C:C0:B8:98:4B:0B:02:45:AB:2C:E9:53:8F:CE:F6:63:D9:9A:93:21:DC:53:22:8A:EE:69:DE" "E8:56:CB:80:E1:A3:18:70:01:7C:B0:0B:58:65:1A:FB:70:6B:B9:30:7C:38:5B:FE:66:CA:9F:5E:06:E5:33:CE")
+subj=("subject= /C=US/ST=Maryland/L=Ellicott City/O=Huntress Labs Inc./CN=*.huntress.io" "subject= /CN=huntresscdn.com" "subject= /C=US/ST=WA/L=Redmond/O=Microsoft Corporation/CN=*.blob.core.windows.net")
+urls=("huntress.io" "huntresscdn.com" "huntressedrue2.blob.core.windows.net")
+
+for i in "${!urls[@]}"; do
+     sha256=$( openssl s_client -connect "${urls[i]}:443" -servername "${urls[i]}" 2> /dev/null < /dev/null | openssl x509 -noout -fingerprint -sha256 | cut -d'=' -f2 )
+     subject=$( openssl s_client -connect "${urls[i]}:443" -servername "${urls[i]}" 2> /dev/null < /dev/null | openssl x509 -noout -subject )
+     if [[ "${hash[i]}" == "$sha256" ]]; then
+          logger "[Certificate validation successful for ${urls[i]}]"
+     else
+          logger "[FAILED: Certificate validation. Hash value does not match!]"
+          logger "Hash that was returned: $sha256"
+          logger "Hash that was expected: ${hash[i]}"
+          logger "Subject that was returned: $subject"
+          logger "Subject that was expected: ${subj[i]}"
+          countFails++
+     fi
+done
 logger ""
 
 
-#test outgoing port 443 connectivity to Huntress
+#test outgoing port 443 connectivity to Huntress URLs
 #output should indicate every URL connection succeeded
-countFails=0
 logger "-- Verifying Huntress services can be reached --"
-for hostn in "update.huntress.io" "huntress.io" "eetee.huntress.io" "eetee.huntresscdn.com" "huntresscdn.com" "huntress-installers.s3.amazonaws.com" "huntress-updates.s3.amazonaws.com" "huntress-uploads.s3.us-west-2.amazonaws.com" "huntress-user-uploads.s3.amazonaws.com" "huntress-rio.s3.amazonaws.com" "huntress-survey-results.s3.amazonaws.com" "huntress-log-uploads.s3.amazonaws.com"; do
+for hostn in "update.huntress.io" "huntress.io" "eetee.huntress.io" "huntresscdn.com" "huntress-installers.s3.amazonaws.com" "huntress-updates.s3.amazonaws.com" "huntress-uploads.s3.us-west-2.amazonaws.com" "huntress-user-uploads.s3.amazonaws.com" "huntress-rio.s3.amazonaws.com" "huntress-survey-results.s3.amazonaws.com" "huntress-log-uploads.s3.amazonaws.com" "agent.huntress.io" "update.huntress.io" "huntressedrue2.blob.core.windows.net" "huntresssiemue2.blob.core.windows.net" "huntresssharedue2.blob.core.windows.net"; do
      nc=$(nc -z -v $hostn 443 2>&1 | grep "succeeded")
      if [ -n "$nc" ]; then
           logger "[Connection to $hostn successful]"
@@ -58,6 +65,8 @@ for hostn in "update.huntress.io" "huntress.io" "eetee.huntress.io" "eetee.huntr
           countFails++
      fi
 done
+logger ""
+
 if [ "$countFails" -gt 0 ]; then
      logger "[FAILED to connect to all Huntress services]"
 else
