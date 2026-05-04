ca4196a9572e1a834cd7c7f81b36ff00c3f420a2
Alan Bishop [Huntress] <109673875+Alan-Huntress@users.noreply.github.com>
2026-05-04 15:08:01 -0400

    Update Test.network.Huntress.sh
    * Pulls list of URL's and expected certificate issuer/subject lines from github. This is to support maintainability so the scripts don't have to be updated when Huntress URLs or certificates change.
    * Also changed much of the script to use the same variables, language, and output as the PowerShell version.

--- macOS/Test.network.Huntress.sh
+++ macOS/Test.network.Huntress.sh
@@ -5,70 +5,127 @@
 # 
 # <<< Bash version >>>
 
-DebugLog="huntress_network_test.log"
+latestUpdate="Huntress Network Tester, macOS and Linux Bash, last updated May 4, 2026"
 
+
+# adds time stamp to a message and then writes that to the log file
+DebugLog="huntress_network_test.log"
 dd=$(date "+%Y-%m-%d  %H:%M:%S")
 logger() {
     echo "$*";
     echo "$dd -- $*" >> $DebugLog;
 }
+logger "-----------------------------------------------------------------------------"
+logger $latestUpdate
+logger "-----------------------------------------------------------------------------"
 
-logger "Huntress Network Tester, macOS and Linux Bash, last updated April 21, 2026"
-logger ""
+# for the summary, keeps track of failures for a conclusive singular pass/fail at the end
 countFails=0
 
-# #test connectivity to Huntress.io on port 80 - output should be <title>Huntress Management Console</title>
-logger "-- Testing DNS resolution and port 80 connectivity"
-curlOutput=$(sudo curl https://huntress.io -s | head -n 14 | tail -n 1)
+# retrieve a list of URLs to test from Huntress github
+URL='https://raw.githubusercontent.com/huntresslabs/support/refs/heads/main/URLdata.json'
+json="$(curl -fsSL --tlsv1.2 "$URL")"
+testURLs=()
+certURLs=()
+expSubject=()
+expIssuer=()
+while IFS= read -r item; do
+     [ -z "$item" ] && continue
+     testURLs+=("$item")
+done < <(echo "$json" | jq -r '.array1[] | select(length > 0)')
+while IFS= read -r item; do
+     [ -z "$item" ] && continue
+     certURLs+=("$item")
+done < <(echo "$json" | jq -r '.array2[] | select(length > 0)')
+# even array indices are Subjects, odd are Issuer
+count=0    
+while IFS= read -r item; do
+     [ -z "$item" ] && continue
+     if (( $count % 2 == 0 )); then
+          expSubject+=("$(echo "$item" | xargs)")
+     else
+          expIssuer+=("$(echo "$item" | xargs)")
+     fi
+     ((count++))
+done < <(echo "$json" | jq -r '.array3[] | select(length > 0)')
+
+
+# Simple test just to establish working DNS and basic internet connectivity
+logger "-- Testing DNS resolution and port 80 connectivity --"
+curlOutput="$(sudo curl -fsS --connect-timeout 5 --max-time 10 "https://huntress.io" 2>&1 | head -n 14 | tail -n 1)"
+status=$?
 if [ "$curlOutput" == "<title>Huntress Management Console</title>" ]; then
      logger "[DNS Resolution / port 80 connection successful]"
 else
-     logger "[FAILED: DNS and port 80 checks]"
-     countFails++
+     logger "[FAILED: DNS and port 80 checks] $curlOutput"
+     ((countFails++))
 fi
 logger ""
 
 
-#tests that the expected certificates are not intercepted. If the expected cert is not returned the agent will not function.
-#output should indicate the cert is for Huntress or Microsoft 
+# tests that the expected certificates are not intercepted. If the expected cert is not returned the agent will not function.
 logger "-- Testing Certificate Validation --"
-hash=("87:5B:E4:BB:1B:08:7F:57:3E:EB:4D:F0:C6:CB:81:1F:07:78:B0:E0:EF:9E:2F:31:84:EA:9F:2A:25:88:47:5C" "75:A9:87:63:3E:2C:C0:B8:98:4B:0B:02:45:AB:2C:E9:53:8F:CE:F6:63:D9:9A:93:21:DC:53:22:8A:EE:69:DE" "E8:56:CB:80:E1:A3:18:70:01:7C:B0:0B:58:65:1A:FB:70:6B:B9:30:7C:38:5B:FE:66:CA:9F:5E:06:E5:33:CE")
-subj=("subject= /C=US/ST=Maryland/L=Ellicott City/O=Huntress Labs Inc./CN=*.huntress.io" "subject= /CN=huntresscdn.com" "subject= /C=US/ST=WA/L=Redmond/O=Microsoft Corporation/CN=*.blob.core.windows.net")
-urls=("huntress.io" "huntresscdn.com" "huntressedrue2.blob.core.windows.net")
+certFailCounter=0
+numEntries=${#certURLs[@]}
+for (( i=0; i<numEntries; i++ )); do
+     cleanURL=$(echo "${certURLs[i]}" | sed -e 's|^[^/]*//||' -e 's|/.*$||')
+     s_client==$(printf '\n' | openssl s_client -connect "${cleanURL}:443" -servername "${cleanURL}" 2> /dev/null < /dev/null )
+     recIssuer=$(printf '%s\n' "$s_client" | openssl x509 -noout -issuer -nameopt compat | cut -d'/' -f2- | xargs)
+     recSubject=$(printf '%s\n' "$s_client" | openssl x509 -noout -subject -nameopt compat | cut -d'/' -f2- | xargs)
 
-for i in "${!urls[@]}"; do
-     sha256=$( openssl s_client -connect "${urls[i]}:443" -servername "${urls[i]}" 2> /dev/null < /dev/null | openssl x509 -noout -fingerprint -sha256 | cut -d'=' -f2 )
-     subject=$( openssl s_client -connect "${urls[i]}:443" -servername "${urls[i]}" 2> /dev/null < /dev/null | openssl x509 -noout -subject )
-     if [[ "${hash[i]}" == "$sha256" ]]; then
-          logger "[Certificate validation successful for ${urls[i]}]"
+     if [[ "$recSubject" == "${expSubject[i]}" ]]; then
+          logger "[Certificate subject validation successful for $cleanURL]"
      else
-          logger "[FAILED: Certificate validation. Hash value does not match!]"
-          logger "Hash that was returned: $sha256"
-          logger "Hash that was expected: ${hash[i]}"
-          logger "Subject that was returned: $subject"
-          logger "Subject that was expected: ${subj[i]}"
-          countFails++
+          ((certFailCounter++))
+          ((countFails++))
+          logger "[FAILED: Subject validation. Certificate does not match for [$cleanURL] !]"
+          logger "Subject that was returned: [$recSubject]"
+          logger "Subject that was expected: [${expSubject[i]}]"
      fi
+
+     if [[ "$recIssuer" == "${expIssuer[i]}" ]]; then
+          logger "[Certificate issuer validation successful for $cleanURL]"
+     else
+          ((certFailCounter++))
+          ((countFails++))
+          logger "[FAILED: Issuer validation. Certificate does not match for [$cleanURL] !]"
+          logger "Subject that was returned: [$recIssuer]"
+          logger "Subject that was expected: [${expIssuer[i]}]"
+     fi
+
 done
+if [[ "$certFailCounter" > 0 ]]; then
+     logger ""
+     logger "------------------------------------------------------------------------------------------------------------------------------"
+     logger "The Subject/Issuer text above usually identifies if this is a DPI/cert interception issue, or a cert chain issue."
+     logger "* If the returned SUBJECT does not contain 'Huntress' or 'Microsoft' in the text this is likely a DPI/cert interception issue."
+     logger "      You'll need to add an exclusion for the certificate for this URL in your DPI/cert interception service: $cleanURL"
+     logger "* If the returned ISSUER does not contain 'DigiCert', 'Google', or 'Microsoft', this is likely a  DPI/cert interception issue."
+     logger "      You'll need to add an exclusion for the certificate for this URL in your DPI/cert interception service: $cleanURL"
+     logger "* Otherwise this is likely a missing certificate chain. Check for pending OS updates, reboot, and try again."
+     logger "------------------------------------------------------------------------------------------------------------------------------"
+fi
 logger ""
 
 
-#test outgoing port 443 connectivity to Huntress URLs
-#output should indicate every URL connection succeeded
+# test outgoing port 443 connectivity to Huntress URLs
 logger "-- Verifying Huntress services can be reached --"
-for hostn in "update.huntress.io" "huntress.io" "eetee.huntress.io" "huntresscdn.com" "huntress-installers.s3.amazonaws.com" "huntress-updates.s3.amazonaws.com" "huntress-uploads.s3.us-west-2.amazonaws.com" "huntress-user-uploads.s3.amazonaws.com" "huntress-rio.s3.amazonaws.com" "huntress-survey-results.s3.amazonaws.com" "huntress-log-uploads.s3.amazonaws.com" "agent.huntress.io" "update.huntress.io" "huntressedrue2.blob.core.windows.net" "huntresssiemue2.blob.core.windows.net" "huntresssharedue2.blob.core.windows.net"; do
-     nc=$(nc -z -v $hostn 443 2>&1 | grep "succeeded")
+for i in "${!testURLs[@]}"; do
+     cleanURL=$(echo "${testURLs[i]}" | sed -e 's|^[^/]*//||' -e 's|/.*$||')
+     nc=$(nc -zvw 5 "$cleanURL" 443 2>&1 | grep "succeeded")
      if [ -n "$nc" ]; then
-          logger "[Connection to $hostn successful]"
+          logger "[Connection to $cleanURL successful]"
      else
-          logger "[FAILED: Connection to $hostn]"
-          countFails++
+          logger "[FAILED: Connection to $cleanURL"
+          ((countFails++))
      fi
 done
 logger ""
 
 if [ "$countFails" -gt 0 ]; then
      logger "[FAILED to connect to all Huntress services]"
+     logger "------------------------ FAILED network test ----------------------------------"
 else
      logger "[Successfully connected to Huntress services]"
+     logger "---------------------- Network testing complete --------------------------------"
 fi
