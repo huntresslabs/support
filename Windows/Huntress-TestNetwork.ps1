1b415043a5231f4cbf4f285a575bfa80b97992b5
Alan Bishop [Huntress] <109673875+Alan-Huntress@users.noreply.github.com>
2026-05-04 15:02:46 -0400

    Update Huntress-TestNetwork.ps1
    * Pulls list of URL's and expected certificate issuer/subject lines from github. This is to support maintainability so the scripts don't have to be updated when Huntress URLs or certificates change.
    * Also changed much of the script to use the same variables, language, and output as the Bash version.

--- Windows/Huntress-TestNetwork.ps1
+++ Windows/Huntress-TestNetwork.ps1
@@ -1,56 +1,145 @@
-# This script tests that HTTPS connectivity can be established to the Huntress cloud and that communication is not intercepted (certificate mismatch)
-# Output will be printed to console as well as saved in \Windows\temp\HuntressNetworkTest.txt
+# Tests a number of ways Huntress agents communicate with the Huntress portal
+# Output is to standard out as well as the file represented by $DebugLog
+# 
+# <<< PowerShell version >>>
 
-$latestUpdate = "Huntress Network Tester, Windows PowerShell, last updated: April 21, 2026"
+$latestUpdate = "Huntress Network Tester, Windows PowerShell, last updated: May 4, 2026"
 
 
-$DebugLog = "c:\Windows\temp\huntress_network_test.log"
 # adds time stamp to a message and then writes that to the log file
-function LogMessage ($msg) {
+$DebugLog = "c:\Windows\temp\huntress_network_test.log"
+function logger ($msg) {
 	$TimeStamp = "[{0:yyyy/MM/dd} {0:HH:mm:ss}]" -f (Get-Date)
 	Add-Content $DebugLog "$TimeStamp $msg"
-    Write-Output "$TimeStamp $msg"
+    Write-Output "$msg"
 }
-LogMessage "-----------------------------------------------------------------------------"
-LogMessage $latestUpdate
-LogMessage "-----------------------------------------------------------------------------"
+logger "-----------------------------------------------------------------------------"
+logger $latestUpdate
+logger "-----------------------------------------------------------------------------"
+
+# for the summary, keeps track of failures for a conclusive singular pass/fail at the end
+$countFails=0
 
 # Avoid "First Run Customize" blocking the testing by disabling it
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize" -Value 2
 # Force TLS 1.2 to avoid compatibility issues and ensure accurate testing (Huntress uses TLS 1.2+ only)
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+try {
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+}
+catch {
+    Write-Error "Huntress requires TLS 1.2 or higher for security reasons."
+    exit 1
+}
+
+# retrieve a list of URLs to test from Huntress github
+$URL = 'https://raw.githubusercontent.com/huntresslabs/support/refs/heads/main/URLdata.json'
+try {
+    $data = Invoke-RestMethod -Uri $URL -UseBasicParsing -ErrorAction Stop
+} catch {
+    logger "Fallback using WebClient (still uses TLS 1.2)"
+    $wc = New-Object System.Net.WebClient
+    $wc.Headers['User-Agent'] = 'HuntressSupportScript'
+    $jsonString = $wc.DownloadString($URL)
+    $data = $jsonString | ConvertFrom-Json
+}
+
+# process the data from github
+$data = (Invoke-WebRequest -Uri $URL -UseBasicParsing -ErrorAction Stop).Content | ConvertFrom-Json
+$testURLs   = @($data.array1)
+$certURLs   = @($data.array2)
+$certTemp   = @($data.array4)
+$expSubject = @()
+$expIssuer  = @()
+# array4 contains two different sets of info, even indices are subject, odd indices are issuer
+for ($i = 0; $i -lt $certTemp.Count; $i++) {
+    if ($i % 2 -eq 0) {
+        $expSubject += $certTemp[$i]
+    } else {
+        $expIssuer += $certTemp[$i]
+    }
+}
 
 
-# Testing for blocked port 443
-$file_name  = "96bca0cef10f45a8f7cf68c4485f23a4.txt"
-$file_name2 = "96bca0cef10f45a8f7cf68c4485f23a4.txt?sp=r&st=2026-03-06T01:03:27Z&se=3026-03-06T09:18:27Z&spr=https&sv=2024-11-04&sr=b&sig=9gZx9aselhUP0SjeYvYSXD14S5QMpxkD1F0LMv0UBS0%3D"
-$file_name3 = "96bca0cef10f45a8f7cf68c4485f23a4.txt?sp=r&st=2026-03-06T01:08:31Z&se=3026-03-06T09:23:31Z&spr=https&sv=2024-11-04&sr=b&sig=TgKJlXd7Q0ggdX5DN7DHfpXHUMLVZdujzZS5%2FcCWgIs%3D"
-$file_name4 = "96bca0cef10f45a8f7cf68c4485f23a4.txt?sp=r&st=2026-03-06T01:04:57Z&se=3026-03-06T09:19:57Z&spr=https&sv=2024-11-04&sr=b&sig=TczBN0PJ8F375iD3xzgB0j%2BZLgJ6Q8LsS2IR0kuGPFQ%3D"
-$file_name5 = "96bca0cef10f45a8f7cf68c4485f23a4.txt?sp=r&st=2026-03-06T01:05:16Z&se=3026-03-06T09:20:16Z&spr=https&sv=2024-11-04&sr=b&sig=wop9nqdC255Fbe84mlgz98cHUZ8Q1qFr%2BEOcyZsFEsM%3D"
-$URLs = @(("https://update.huntress.io/agent/connectivity/{0}" -f $file_name),
-		  ("https://huntress.io/agent/connectivity/{0}" -f $file_name),
-		  ("https://eetee.huntress.io/{0}"-f $file_name),
-		  ("https://huntresscdn.com/agent/connectivity/{0}" -f $file_name),
-		  ("https://huntress-installers.s3.amazonaws.com/agent/connectivity/{0}" -f $file_name),
-		  ("https://huntress-updates.s3.amazonaws.com/agent/connectivity/{0}" -f $file_name),
-		  ("https://huntress-uploads.s3.us-west-2.amazonaws.com/agent/connectivity/{0}" -f $file_name),
-		  ("https://huntress-user-uploads.s3.amazonaws.com/agent/connectivity/{0}" -f $file_name),
-		  ("https://huntress-rio.s3.amazonaws.com/agent/connectivity/{0}" -f $file_name),
-		  ("https://huntress-survey-results.s3.amazonaws.com/agent/connectivity/{0}" -f $file_name),
-		  ("https://huntress-log-uploads.s3.amazonaws.com/agent/connectivity/{0}" -f $file_name),
-		  ("https://agent.huntress.io/agent/connectivity/{0}" -f $file_name),
-		  ("https://update.huntress.io/agent/connectivity/{0}" -f $file_name),
-		  ("https://huntressedrue2.blob.core.windows.net/huntress-installers/agent/connectivity/{0}" -f $file_name2),
-		  ("https://huntresssiemue2.blob.core.windows.net/huntress-log-uploads/agent/connectivity/{0}" -f $file_name3),
-		  ("https://huntresssharedue2.blob.core.windows.net/huntress-uploads/agent/connectivity/{0}" -f $file_name4))
+# Simple test just to establish working DNS and basic internet connectivity
+logger "-- Testing DNS resolution and port 80 connectivity --"
+$curlOutput = $(curl "https://huntress.io" -UseBasicParsing)
+if ($curlOutput.StatusCode -eq 200) {
+    $curlOutput = $($curlOutput.Content) | Select-Object -First 14 | Select-Object -Last 1
+    $startIndex = $curlOutput.IndexOf("<title>")
+    if ($startIndex -ne -1) {
+        $contentStart = $startIndex + 7
+        $result = $curlOutput.Substring($contentStart, 27)
+        logger "[DNS Resolution / port 80 connection successful]"
+    } else {
+        Write-Host "The tag '<title>' was not found."
+        $countFails++
+    }
+} else {
+    logger "[FAILED: DNS and port 80 checks]"
+    echo "Output: $curlOutput"
+    $countFails++
+}
+logger ""
 
-LogMessage "Checking for HTTPS connectivity with Huntress cloud servers:"
-foreach ($URL in $URLs) {
+
+# tests that the expected certificates are not intercepted. If the expected cert is not returned the agent will not function.
+logger "-- Testing Certificate Validation --"
+$failCounter = 0
+for ($i = 0; $i -lt $certURLs.Count; $i++) {
+    $cleanURL = ($certURLs[$i] -replace '^https://', '') -replace '/.*',''
+    $uri = ([uri]$cleanURL)
+    $tcp = New-Object Net.Sockets.TcpClient
+    $tcp.Connect("$uri", 443)
+    $ssl = New-Object Net.Security.SslStream($tcp.GetStream(),$false,{$true})
+    $ssl.AuthenticateAsClient($uri)
+    $cert       = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 $ssl.RemoteCertificate
+    $recSubject = $cert.Subject
+    $recIssuer  = $cert.Issuer
+
+    if ($recSubject -eq $expSubject[$i]) {
+        logger "[Certificate subject validation successful for $cleanURL]"
+    } else {
+        logger "[FAILED: Subject validation. Certificate does not match for [$cleanURL] !]"
+        logger "Subject that was returned: [$recSubject]"
+        logger "Subject that was expected: [$($expSubject[$i])]"
+        $failCounter++
+        $countFails++
+    }
+
+     if ($recIssuer -eq $expIssuer[$i]) {
+        logger "[Certificate issuer validation successful for $cleanURL]"
+     } else {
+        logger "[FAILED: Issuer validation. Certificate does not match for [$cleanURL] !]"
+        logger "Subject that was returned: [$recIssuer]"
+        logger "Subject that was expected: [$($expIssuer[$i])]"
+        $failCounter++
+        $countFails++
+    }
+    $ssl.Dispose()
+    $tcp.Close()
+}
+if ($failCounter -gt 0) {
+     logger ""
+     logger "------------------------------------------------------------------------------------------------------------------------------"
+     logger "The Subject/Issuer text above usually identifies if this is a DPI/cert interception issue, or a cert chain issue."
+     logger "* If the returned SUBJECT does not contain 'Huntress' or 'Microsoft' in the text this is likely a DPI/cert interception issue."
+     logger "      You'll need to add an exclusion for the certificate for this URL in your DPI/cert interception service: $cleanURL"
+     logger "* If the returned ISSUER does not contain 'DigiCert', 'Google', or 'Microsoft', this is likely a  DPI/cert interception issue."
+     logger "      You'll need to add an exclusion for the certificate for this URL in your DPI/cert interception service: $cleanURL"
+     logger "* Otherwise this is likely a missing certificate chain. Check for pending OS updates, reboot, and try again."
+     logger "------------------------------------------------------------------------------------------------------------------------------"
+}
+logger ""
+
+ 
+# test outgoing port 443 connectivity to Huntress URLs
+logger "-- Testing DNS resolution and port 80 connectivity --"
+foreach ($testURL in $testURLs) {
     $StatusCode = 0
     try {
-        $Response = Invoke-WebRequest -Uri $URL -TimeoutSec 5 -ErrorAction Stop -ContentType "text/plain" -UseBasicParsing
+        $uri = ([uri]$testURL)
+        $Response   = Invoke-WebRequest -Uri $testURL -TimeoutSec 5 -ErrorAction Stop -ContentType "text/plain" -UseBasicParsing
         $StatusCode = $Response.StatusCode
-
         # Convert from bytes, if necessary
         if ($Response.Content.GetType() -eq [System.Byte[]]) {
             $StrContent = [System.Text.Encoding]::UTF8.GetString($Response.Content)
@@ -59,48 +148,27 @@ foreach ($URL in $URLs) {
         }
         $StrContent = [string]::join("",($StrContent.Split("`n")))
     } catch {
-        LogMessage "Error: $($_.Exception.Message)"
+        logger "Error: $($_.Exception.Message)"
+        $countFails++
     }
 
-    $shortURL = (($URL.Split('/'))[0..2] -join '/')
+    $cleanURL = (($testURL.Split('/'))[0..2] -join '/')
     if ($StatusCode -ne 200) {
-        LogMessage = "WARNING, connectivity to Huntress URL's is being interrupted. You MUST open port 443 for $shortURL in order for the Huntress agent to function. Status code: $StatusCode"
+        logger = "WARNING, connectivity to Huntress URL's is being interrupted. You MUST open port 443 for $cleanURL in order for the Huntress agent to function. Status code: $StatusCode"
+        $countFails++
     } elseif ( ! ($StrContent -eq "96bca0cef10f45a8f7cf68c4485f23a4")) {
-        LogMessage = "WARNING, successful connection to Huntress URL, however, content did not match expected. Ensure no proxy or content filtering is preventing access!"
+        logger = "WARNING, successful connection to Huntress URL, however, content did not match expected. Ensure no proxy or content filtering is preventing access!"
+        $countFails++
     } else {
-        LogMessage "Connection succeeded to $shortURL"
+        logger "[Connection to $cleanURL successful]"
     }
 }
-LogMessage "-----------------------------------------------------------------------------"
+logger ""
 
-
-LogMessage "Testing for certificate interception/inspection/deep packet inspection"
-$URLs = @("https://www.huntress.io/agent/connectivity/96bca0cef10f45a8f7cf68c4485f23a4.txt",
-		  "https://huntresscdn.com/agent/connectivity/96bca0cef10f45a8f7cf68c4485f23a4.txt",
-		  "https://huntressedrue2.blob.core.windows.net/huntress-installers/agent/connectivity/96bca0cef10f45a8f7cf68c4485f23a4.txt?sp=r&st=2026-03-06T01:03:27Z&se=3026-03-06T09:18:27Z&spr=https&sv=2024-11-04&sr=b&sig=9gZx9aselhUP0SjeYvYSXD14S5QMpxkD1F0LMv0UBS0%3D")
-$hash = @("95F9708BE32E778CE4ED11CF9A7DBC50041663A4",
-		  "D80720CE9322EFECB54144CA775BADAC5A530C61",
-		  "C0D452A62049904D88261B11AD1185FF706E072D")
-
-for ($i = 0; $i -lt $URLs.Count; $i++) {
-	$url = [System.Uri]$($URLs[$i])
-	$shortURL = (($URLs[$i].Split('/'))[0..2] -join '/')
-	$Connection = [System.Net.HttpWebRequest]::Create($url)
-	$Response = $Connection.GetResponse()
-	$certResponse = $Connection.ServicePoint.Certificate
-	$hashResponse = $certResponse.GetCertHashString()
-	$Response.Dispose()
-
-	if ( $hash[$i] -eq $hashResponse ) {
-		LogMessage "Certificate match for $shortURL!"
-	} else {
-		LogMessage "WARNING: Certificate doesn't match! Possible DPI, cert interception or pinning of certificates. The certificate from this URL must match otherwise the Huntress agent will not function!"
-		LogMessage "This is likely the certificate interceptor: $($certResponse.Subject) $($certResponse.Issuer)"
-		LogMessage "Expected: $($hash[$i])"
-		LogMessage "Received: $hashResponse"
-	}
+if ($countFails -gt 0) {
+    logger "[FAILED to connect to all Huntress services]"
+    logger "------------------------ FAILED network test ----------------------------------"
+} else {
+    logger "[Successfully connected to Huntress services]"
+    logger "---------------------- Network testing complete --------------------------------"
 }
-LogMessage ""
-
-
-LogMessage "---------------------- Network testing complete --------------------------------"
